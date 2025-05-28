314200c414f4df5f8ff79198e9359f374bef9edd
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2025-05-28 10:25:46 +0900

    reports作成

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
+import { fetchHistoryData } from "./fetch-history-data";
+import { submitData } from "./submit-data";
+
+//設定情報
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+
+  //Cloud Firestoreから取得したデータを表示する
+  if(document.getElementById("js-history")) {
+    fetchHistoryData(getDocs, collection, db);
+  }
+
+
+//Cloud Firestoreにデータ送信する
+if(document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection ,db));
+}
